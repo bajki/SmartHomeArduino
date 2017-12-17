c486b5f4eed363531728ea4517d1dad5f95a159e
coolplace <coolplace@abv.bg>
2017-12-17 09:05:18 +0200

    working relays and DHT

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include "DHT.h"
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-#define DHT22SensorPin D5
+
+// #define DHTSensorPin D5
+#define DHTPIN            D5
+// Uncomment the type of sensor in use:
+#define DHTTYPE           DHT11     // DHT 11 
+// #define DHTTYPE           DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE           DHT21     // DHT 21 (AM2301)
+
 
 ESP8266WebServer server(80);
-DHT dht;
+DHT_Unified dht(DHTPIN, DHTTYPE);
 
 const char* ssid = "VIVACOM_IVAN";
 const char* password =  "20162016";
 
-int pins[] = { LED_BUILTIN, D7, D8, D9, D0, D1 };   
-int pinStatuses[] = {LOW, LOW, LOW, LOW, LOW, LOW};
-String pinKeys[] = { "r1","r2","r3","r4","r5","r6"};         
-int pinCount = 6;
+int pins[] = { LED_BUILTIN, D6, D7 };   
+int pinStatuses[] = {LOW, LOW, LOW};
+String pinKeys[] = { "r1","r2","r3"};         
+int pinCount = 3;
+
 
 void handleStatus(){
     String message = "{\"Error\":\"\", ";
@@ -25,8 +35,16 @@ void handleStatus(){
         message += ", ";
     }
     
-    float h = dht.getHumidity();
-    float t = dht.getTemperature();
+    // float h = dht.getHumidity();
+    // float t = dht.getTemperature();
+
+
+    sensors_event_t event;  
+    dht.temperature().getEvent(&event);
+    float t = event.temperature;
+    dht.humidity().getEvent(&event);
+    float h =event.relative_humidity;
+
     message+= "\"t\":";
     message+= t;
     message +=", \"h\":";
@@ -54,12 +72,13 @@ void handleSwitch(){
                 break;
             }
         }
+
     if(relay == -1){
         sendErrorResponce("WrongSchemaError");
         return;
     }
     int newRelayStatus = server.arg("status").toInt();
-    digitalWrite(pins[relay], newRelayStatus==0 ? LOW : HIGH);
+    digitalWrite(pins[relay], newRelayStatus);
     pinStatuses[relay] = newRelayStatus;
 
     handleStatus();
@@ -67,14 +86,14 @@ void handleSwitch(){
 
 void handleCommand() {
       if (server.hasArg("command")== false){ //Check if body received
-            sendErrorResponce("NoCommandError");
-            return;
+             sendErrorResponce("NoCommandError");
+             return;
         }
         String command = server.arg("command");
         if(command.equals("switchrelay")){
-            handleSwitch();
+                handleSwitch();
         } else {
-            handleStatus();
+                handleStatus();
         }
 }
  
@@ -95,12 +114,12 @@ void setup() {
     for(int i=0; i<pinCount; i++){
         pinMode(pins[i], OUTPUT);
     }
-    // for(int i=0; i<pinCount; i++){
-        // digitalWrite(pins[i], pinStatuses[i]);
-    // }
+    for(int i=0; i<pinCount; i++){
+        digitalWrite(pins[i], pinStatuses[i]);
+    }
 
     Serial.println("Init DHT");
-    dht.setup(DHT22SensorPin); // data pin 2
+    dht.begin();
     delay(3000);
 }
  
